c2dd4c4929107970e935515bfef92075192db4a4
Ayush Garg <62711517+ayush4921@users.noreply.github.com>
2021-06-26 23:40:52 +0800

    commit

--- static/index/index.js
+++ static/index/index.js
@@ -24,7 +24,7 @@ $("#submit_button").click(function (event) {
           email: user.email,
         },
         type: "POST",
-        url: "http://127.0.0.1:5000/makeqrcodeandsetupfirebase",
+        url: "https://reformbar.herokuapp.com/makeqrcodeandsetupfirebase",
       }).done(function (data) {
         $("#form1").hide();
         $("#result").html(
--- static/qr_code.js
+++ static/qr_code.js
@@ -28,7 +28,7 @@ docReady(function () {
           password: "36fb75181c26195f01aff5144aa1464b",
         },
         type: "POST",
-        url: "http://127.0.0.1:5000/add_drink",
+        url: "https://reformbar.herokuapp.com/add_drink",
       }).done(function (data) {
         console.log("Request sent");
         alert(data);
